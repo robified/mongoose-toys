cc7c708e13d994da6afbb82c014a629fe98e5f8e
Robin <daorob@gmail.com>
2020-04-11 01:30:18 -0500

    Install/configure/require mongoose package. Connecting to local MongoDB successfully.

---
+++ config/database.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/toys', {
+    // these just come from the warnings, googling it, or the docs
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+});
+
+// shortcut to mongoose.connection object
+const db = mongoose.connection;
+
+db.on('connected', function () {
+    console.log(
+        `Connected on MongoDB to the ${db.name} database on ${db.host}:${db.port}.`
+    );
+});
